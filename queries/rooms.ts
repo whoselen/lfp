2fe92c0e8470da30c1d2c9f959fa524bdfadc31d
whoslen-alt <benkyoumasho@gmail.com>
2025-02-18 21:40:51 +0800

    recent rooms order

--- queries/rooms.ts
+++ queries/rooms.ts
@@ -1,24 +1,29 @@
 import { TypedSupabaseClient } from "@/utils/types";
 
 const getRecentRooms = (client: TypedSupabaseClient) => {
-  return client.from("rooms").select(`
-  id,
-  title,
-  description,
-  user_id,
-  game_id,
-  rank_id,
-  max_participants,
-  room_accessibility_tools (
-    tool_id
-  ),
-  room_game_servers (
-    server_id
-  ),
-  room_tags (
-    tag_id
-  )
-`);
+  return client
+    .from("rooms")
+    .select(
+      `
+        id,
+        title,
+        description,
+        user_id,
+        game_id,
+        rank_id,
+        max_participants,
+        room_accessibility_tools (
+          tool_id
+        ),
+        room_game_servers (
+          server_id
+        ),
+        room_tags (
+          tag_id
+        )
+      `
+    )
+    .order("inserted_at", { ascending: false });
 };
 
 export { getRecentRooms };
